21dce8769e2aef42e31cedc312bb1acf29ea7d8e
Daniel Moore <danmr4@gmail.com>
2013-08-04 21:58:21 -0400

    Initial Commit

---
+++ View Switcher/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  View Switcher
+//
+//  Created by daniel moore on 8/4/13.
+//  Copyright (c) 2013 daniel moore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ View Switcher/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  View Switcher
+//
+//  Created by daniel moore on 8/4/13.
+//  Copyright (c) 2013 daniel moore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BIDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BIDAppDelegate class]));
+    }
+}
